25a3a4052cd84177bcd7dd7c00ddfdb49e02e9f1
jeong <chan3358@naver.com>
2023-04-19 14:29:15 +0900

    feat: add no items found to CardList

--- components/card/CardList.tsx
+++ components/card/CardList.tsx
@@ -1,11 +1,21 @@
 import { ParsedDatabaseItem } from "@/utils/parseDatabaseItems";
 import CardItem from "./CardItem";
+import { MdOutlineSearchOff } from "react-icons/md";
 
 interface CardListProps {
   cardItems: ParsedDatabaseItem[];
 }
 
 function CardList({ cardItems }: CardListProps) {
+  if (cardItems.length === 0) {
+    return (
+      <div className="flex flex-col justify-center items-center">
+        <MdOutlineSearchOff size={"4rem"} />
+        <p className="text-2xl font-bold">No Items Found!</p>
+      </div>
+    );
+  }
+
   return (
     <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
       {cardItems.map((cardItem) => (
