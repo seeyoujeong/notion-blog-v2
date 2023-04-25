9a9e118227f8df380a25bea7ddd4d9d6be978263
jeong <chan3358@naver.com>
2023-04-25 11:38:30 +0900

    design: update HeroSection

--- components/intro/HeroSection.tsx
+++ components/intro/HeroSection.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { BsEmojiSmile } from "react-icons/bs";
 
 function HeroSection() {
   return (
@@ -7,21 +8,21 @@ function HeroSection() {
         <div className="relative">
           <span className="absolute left-1/2 -bottom-2 -translate-x-1/2 rotate-45 bg-black w-6 h-6" />
           <span className="font-bold text-white bg-black py-2 px-4 rounded-lg relative">
-            WOW! 와우! わう!
+            hello? see you!
           </span>
         </div>
 
-        <h2 className="font-black text-6xl leading-[1.2] break-keep md:max-w-md">
-          Hello World! 헬로, 월드! ハロー, ワールド!
-        </h2>
+        <div className="flex justify-center md:max-w-md md:relative md:left-10">
+          <BsEmojiSmile size={"4rem"} />
+        </div>
 
-        <p className="font-light text-xl text-gray-400 md: max-w-xl break-keep">
+        <p className="font-light text-xl text-gray-600 md:max-w-xl break-keep">
           공부한 내용을 정리하는 블로그입니다. 대부분 컴퓨터 관련 내용일 거
           같습니다.
         </p>
 
         <Link href={"/about"}>
-          <button className="px-4 py-2 border border-black rounded-3xl font-semibold w-28 mx-auto md:mx-0 hover:bg-black hover:text-white">
+          <button className="px-4 py-2 border-2 border-black rounded-3xl font-semibold w-28 mx-auto md:mx-0 hover:bg-black hover:text-white">
             About Me
           </button>
         </Link>
