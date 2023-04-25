86c5313a603f780fa650ca049b5d5d964da293b1
jeong <chan3358@naver.com>
2023-04-25 10:49:21 +0900

    feat: add Pagination to common

--- components/intro/CardSection.tsx
+++ components/intro/CardSection.tsx
@@ -1,5 +1,7 @@
+import { ITEMS_PER_PAGE } from "@/const/const";
 import { ParsedDatabaseItem } from "@/utils/parseDatabaseItems";
 import CardList from "../card/CardList";
+import Pagination from "../common/Pagination";
 
 interface CardSectionProps {
   cardItems: ParsedDatabaseItem[];
@@ -12,6 +14,7 @@ function CardSection({ cardItems, totalLength }: CardSectionProps) {
       <div className="max-w-5xl w-4/5 mx-auto flex flex-col gap-6 py-8 px-4">
         <h3 className="font-bold text-3xl">Posts</h3>
         <CardList cardItems={cardItems} />
+        <Pagination totalPage={Math.ceil(totalLength / ITEMS_PER_PAGE)} />
       </div>
     </section>
   );
