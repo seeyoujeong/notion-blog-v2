d7718b59b022fd428f701cbd841a5fcf21644e77
jeong <chan3358@naver.com>
2023-04-25 12:02:38 +0900

    chore: update Card, HeroSection and ITEM_PER_PAGE

--- components/intro/CardSection.tsx
+++ components/intro/CardSection.tsx
@@ -11,7 +11,7 @@ interface CardSectionProps {
 function CardSection({ cardItems, totalLength }: CardSectionProps) {
   return (
     <section>
-      <div className="max-w-5xl w-4/5 mx-auto flex flex-col gap-6 py-8 px-4">
+      <div className="max-w-5xl w-4/5 mx-auto flex flex-col gap-6 pt-2 pb-8 px-4">
         <h3 className="font-bold text-3xl">Posts</h3>
         <CardList cardItems={cardItems} />
         <Pagination totalPage={Math.ceil(totalLength / ITEMS_PER_PAGE)} />
--- components/intro/HeroSection.tsx
+++ components/intro/HeroSection.tsx
@@ -1,24 +1,26 @@
 import Link from "next/link";
-import { BsEmojiSmile } from "react-icons/bs";
 
 function HeroSection() {
   return (
     <section>
-      <div className="flex flex-col md:items-start gap-8 py-16 px-4 w-4/5 max-w-5xl mx-auto text-center md:text-left">
+      <div className="flex flex-col md:items-start gap-8 py-12 px-4 w-4/5 max-w-5xl mx-auto text-center md:text-left">
         <div className="relative">
           <span className="absolute left-1/2 -bottom-2 -translate-x-1/2 rotate-45 bg-black w-6 h-6" />
           <span className="font-bold text-white bg-black py-2 px-4 rounded-lg relative">
-            hello? see you!
+            see you!
           </span>
         </div>
 
+        <h2 className="font-black text-6xl leading-[1.2] break-keep">
+          Hello ワールド!
+        </h2>
+        {/* 
         <div className="flex justify-center md:max-w-md md:relative md:left-10">
           <BsEmojiSmile size={"4rem"} />
-        </div>
+        </div> */}
 
         <p className="font-light text-xl text-gray-600 md:max-w-xl break-keep">
-          공부한 내용을 정리하는 블로그입니다. 대부분 컴퓨터 관련 내용일 거
-          같습니다.
+          공부한 내용을 정리하는 블로그입니다. 대부분 컴퓨터 관련 내용...ㅎ
         </p>
 
         <Link href={"/about"}>
