1b3b504cc0944d4898185d51b6bf5e92a7d14058
jeong <chan3358@naver.com>
2023-05-03 14:40:42 +0900

    feat: add social profiles

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,8 +1,29 @@
+import Link from "next/link";
+import { AiOutlineGithub, AiOutlineInstagram } from "react-icons/ai";
+
 function Footer() {
   return (
     <footer className="bg-gray-100">
-      <div className="text-gray-600 text-center py-8">
-        &copy; 2023 seeyoujeong All Rights Reserved.
+      <div className="flex flex-col sm:flex-row gap-2 sm:gap-0 justify-between items-center max-w-5xl mx-auto py-4 sm:p-8">
+        <div className="text-gray-600">
+          &copy; 2023 seeyoujeong All rights reserved.
+        </div>
+        <ul className="flex flex-row gap-2 items-center text-gray-600">
+          <li key="github">
+            <Link href={"https://github.com/seeyoujeong"} passHref>
+              <a target="_blank" rel="noopener noreferroer">
+                <AiOutlineGithub size={"2rem"} />
+              </a>
+            </Link>
+          </li>
+          <li key="instagram">
+            <Link href={"https://www.instagram.com/seeyoujeong"} passHref>
+              <a target="_blank" rel="noopener noreferroer">
+                <AiOutlineInstagram size={"2rem"} />
+              </a>
+            </Link>
+          </li>
+        </ul>
       </div>
     </footer>
   );
