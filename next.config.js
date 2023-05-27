1b40662cf653c7ab472f0b096e71fc609d58b93d
jeong <chan3358@naver.com>
2023-05-27 11:11:57 +0900

    chore: update next.config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.notion.so", "s3.us-west-2.amazonaws.com"],
+    domains: [
+      "www.notion.so",
+      "s3.us-west-2.amazonaws.com",
+      "images.unsplash.com",
+    ],
   },
 };
 
